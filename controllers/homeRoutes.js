efa5e12acf1e73c12aea8961bedd58183f97670b
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-11-10 21:52:11 -0600

    modify controllers and api files.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,19 +24,20 @@ router.get('/', async (req, res) => {
 
 router.get('/post/:id', async (req, res) => {
   try {
-    const projectData = await Project.findByPk(req.params.id, {
+    const postData = await Post.findByPk(req.params.id, {
       include: [
+          User,
         {
-          model: User,
-          attributes: ['name'],
+          model: Comment,
+          attributes: ['User'],
         },
       ],
     });
 
-    const project = postData.get({ plain: true });
+    const post = postData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
+    res.render('post', {
+      ...post,
       logged_in: req.session.logged_in
     });
   } catch (err) {
