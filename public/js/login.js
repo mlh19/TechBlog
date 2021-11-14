e41123bbee47663e23a8d994553aeaac1110dad8
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-11-14 13:10:37 -0600

    add comments to code

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,6 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
-
 const email = document.querySelector('#email-login').value.trim();
 const password = document.querySelector('#password-login').value.trim();
 
@@ -29,12 +28,17 @@ const email = document.querySelector('#email-signup').value.trim();
 const password = document.querySelector('#password-signup').value.trim();
 
     if (name && email && password) {
+        // fetch will return the results of passing the name, email, and password
+        // to the /api/users endpoint to save it.
+        // await tells the program to wait unitl the fetch() request assigns a value
+        // back to response. Only then the program will continue execution as normal.
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({ name, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
+        // Success response, likely 200 code.
         if (response.ok) {
             document.location.replace('/dashboard');
         } else {
